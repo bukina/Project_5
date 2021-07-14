51cdbe7d1745ec5292917af8a5f5625e50046064
bukina <bukina.tetiana@gmail.com>
2021-07-14 12:24:37 +0300

    added sections Contacts Form Footer

---
+++ src/js/script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+var map = L.map('map', {
+    center: [50.43692747994574, 30.619796082239485],
+    zoom: 13
+});
+
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png?{foo}', { foo: 'bar', attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' }).addTo(map);
+
+L.marker([50.43692747994574, 30.619796082239485]).addTo(map);
+
+
